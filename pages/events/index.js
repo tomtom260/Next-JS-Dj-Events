457dff22e3ba794dea022776dcf5394884f24155
Thomas Mesfin <thomasmesfin260@gmail.com>
2021-06-19 16:19:49 +0300

    feat: Events List done
    Added a short event list to homepage
    and the full list to /events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,35 @@
+import EventItem from '@/components/EventItem'
+import Link from 'next/link'
 import Layout from '@/components/Layout'
 
-function index() {
+function EventsPage({ events }) {
+  console.log(events)
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 ? (
+        <h3>No Upcoming Events</h3>
+      ) : (
+        <>
+          {events.map(evt => (
+            <EventItem key={evt.id} evt={evt} />
+          ))}
+          <Link href='/events'>
+            <a className='btn-secondary'>View More</a>
+          </Link>
+        </>
+      )}
     </Layout>
   )
 }
 
-export default index
+export default EventsPage
+
+export async function getServerSideProps() {
+  const events = await (await fetch('http://localhost:3000/api/events')).json()
+  return {
+    props: {
+      events,
+    },
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
-import Layout from '../components/Layout'
+import EventItem from '@/components/EventItem'
+import Link from 'next/link'
+import Layout from '@/components/Layout'
 
-function homePage() {
+function homePage({ events }) {
+  console.log(events)
   return (
     <Layout>
-      <div>
-        <h1>Home</h1>
-      </div>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 ? (
+        <h3>No Upcoming Events</h3>
+      ) : (
+        events.map(evt => <EventItem key={evt.id} evt={evt} />)
+      )}
     </Layout>
   )
 }
 
 export default homePage
+
+export async function getServerSideProps() {
+  const events = (
+    await (await fetch('http://localhost:3000/api/events')).json()
+  ).slice(0, 3)
+  return {
+    props: {
+      events,
+    },
+  }
+}
