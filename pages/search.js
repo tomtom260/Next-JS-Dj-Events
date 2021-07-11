ff7338bc6bf67cdafb728ac83701a00950dc5ce4
Thomas Mesfin <thomasmesfin260@gmail.com>
2021-07-11 09:01:11 +0300

    feat:add search page

---
+++ pages/search.js
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import EventItem from '@/components/EventItem'
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+import qs from 'qs'
+import { query } from 'esquery'
+
+function SearchPage({ events }) {
+  const router = useRouter()
+  return (
+    <Layout>
+      <Link href='/events'>Go back</Link>
+      <h1>{`Search Results for ${router.query.term}`}</h1>
+      {events.length === 0 ? (
+        <h3>No Results</h3>
+      ) : (
+        events.map(evt => <EventItem key={evt.id} evt={evt} />)
+      )}
+    </Layout>
+  )
+}
+
+export default SearchPage
+
+export async function getServerSideProps({ query: { term } }) {
+  const query = qs.stringify({
+    _where: {
+      _or: [
+        { name_contains: term },
+        { venue_contains: term },
+        { performers_contains: term },
+        { description_contains: term },
+      ],
+    },
+  })
+  const events = await (
+    await fetch(`${API_URL}/events?${query}_sort=date:ASC`)
+  ).json()
+  return {
+    props: {
+      events,
+    },
+  }
+}
